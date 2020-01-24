a834c6bc607e6e87d9ba8f67f4206b8bb69cd389
Кириллова Екатерина <kirilek@bk.ru>
2020-01-25 02:38:37 +0300

    Updates

---
+++ precache-manifest.db900df4d25103ac49f2546935ce85ec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34df9b4e0f6f530dd1d632743259a49f",
+    "url": "/vk-apps-currency/index.html"
+  },
+  {
+    "revision": "2c16f59ef181f00b4e94",
+    "url": "/vk-apps-currency/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "bb56652a5051ac1bb4b1",
+    "url": "/vk-apps-currency/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "2c16f59ef181f00b4e94",
+    "url": "/vk-apps-currency/static/js/2.5d0e9c2d.chunk.js"
+  },
+  {
+    "revision": "ce028d5e2b0507b0fb507336501c1edf",
+    "url": "/vk-apps-currency/static/js/2.5d0e9c2d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bb56652a5051ac1bb4b1",
+    "url": "/vk-apps-currency/static/js/main.b7d292c4.chunk.js"
+  },
+  {
+    "revision": "dae897d31523e3918d19",
+    "url": "/vk-apps-currency/static/js/runtime-main.2c852996.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vk-apps-currency/static/media/persik.4e1ec840.png"
+  }
+]);
